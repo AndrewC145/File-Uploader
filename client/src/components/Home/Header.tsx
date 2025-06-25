7abdcc4633707ad15138801fe1c6dc440fe11022
Andrew Cao <andrewcao618@gmail.com>
2025-06-24 20:24:31 -0400

    Added Storage Button

--- client/src/components/Home/Header.tsx
+++ client/src/components/Home/Header.tsx
@@ -19,19 +19,28 @@ function Header() {
           </h1>
         </Link>
         <div className="space-x-4">
-          <HeaderButton
-            className="bg-gray-200 px-3 py-2 text-sm text-black hover:bg-gray-300"
-            text="Sign Up"
-            link="/register"
-          />
           {user !== null ? (
-            <LogoutButton />
+            <>
+              <HeaderButton
+                className="bg-gray-200 px-3 py-2 text-sm text-black hover:bg-gray-300"
+                text="Storage"
+                link="/storage"
+              />
+              <LogoutButton />
+            </>
           ) : (
-            <HeaderButton
-              className="bg-blue-400 px-3 py-2 text-white hover:bg-blue-500"
-              text="Login"
-              link="/login"
-            />
+            <>
+              <HeaderButton
+                className="bg-gray-200 px-3 py-2 text-sm text-black hover:bg-gray-300"
+                text="Sign Up"
+                link="/register"
+              />
+              <HeaderButton
+                className="bg-blue-400 px-3 py-2 text-white hover:bg-blue-500"
+                text="Login"
+                link="/login"
+              />
+            </>
           )}
         </div>
       </nav>
