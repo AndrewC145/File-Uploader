43582b44ac5429dda5ec6248bdf12ff2f3e759b7
Andrew Cao <andrewcao618@gmail.com>
2025-07-17 21:37:54 -0400

    Reorganized Folder Home

--- client/src/components/storage/FolderHome.tsx
+++ client/src/components/storage/FolderHome.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect, useContext, useCallback } from "react";
 import axios from "axios";
 import UserContext from "@/context/userContext";
+import File from "./File";
 
 function FolderHome() {
   const { user } = useContext(UserContext);
@@ -19,7 +20,6 @@ function FolderHome() {
         },
         withCredentials: true,
       });
-      console.log(response);
       setFiles(response.data.homeFiles);
     } catch (error: any) {
       console.error("Error fetching home files:", error);
@@ -31,23 +31,7 @@ function FolderHome() {
     fetchHomeFiles();
   }, [fetchHomeFiles]);
 
-  return (
-    <>
-      {files.map((file) => {
-        return (
-          <div
-            key={file.id}
-            className="flex items-center justify-between px-3 py-2 hover:bg-gray-100 md:px-5 xl:px-7"
-          >
-            <h3 className="flex-[2]">{file.name}</h3>
-            <p className="flex-1">{file.metadata.mimetype}</p>
-            <p className="flex-1">{file.metadata.size} bytes</p>
-            <p className="flex-1">{new Date(file.metadata.lastModified).toLocaleDateString()}</p>
-          </div>
-        );
-      })}
-    </>
-  );
+  return <File files={files} />;
 }
 
 export default FolderHome;
