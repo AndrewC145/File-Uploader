fc8e3f1a1a56c39de921294e70b136d8e7394b93
Andrew Cao <andrewcao618@gmail.com>
2025-07-30 21:21:42 -0400

    Styled Form for Responsiveness

--- client/src/components/Forms/FormTemplate.tsx
+++ client/src/components/Forms/FormTemplate.tsx
@@ -10,9 +10,9 @@ function FormTemplate({ title, children, onSubmit }: FormTemplateProps) {
       <form
         onSubmit={onSubmit}
         method="POST"
-        className="flex w-[30%] flex-col space-y-4 rounded-2xl border-2 border-blue-300 p-4 sm:w-[35%] sm:space-y-5 sm:p-6 md:w-[40%] md:space-y-6 md:p-8 lg:px-10 xl:space-y-8 xl:px-12 2xl:w-[30%]"
+        className="flex w-[70%] flex-col space-y-4 rounded-2xl border-2 border-blue-300 p-4 sm:w-[45%] sm:space-y-5 sm:p-6 md:w-[40%] md:space-y-6 md:p-8 lg:w-[30%] lg:px-10 xl:space-y-8 xl:px-12"
       >
-        <h1 className="text-center text-2xl">{title}</h1>
+        <h1 className="text-center text-lg sm:text-xl md:text-2xl">{title}</h1>
         {children}
       </form>
     </main>
