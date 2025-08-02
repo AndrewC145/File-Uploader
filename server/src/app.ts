a0c644b15bbd0dd7143f1a939f3daccd2e8132ca
Andrew Cao <andrewcao618@gmail.com>
2025-08-01 20:11:41 -0400

    Added Secure

--- server/src/app.ts
+++ server/src/app.ts
@@ -35,7 +35,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 },
+    cookie: { secure: true, maxAge: 1000 * 60 * 60 * 24 },
   })
 );
 
